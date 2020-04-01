96d905b841ddc804dbcf5897214c53880f143cd3
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 00:24:15 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :students do |c|
-      c.integer = 
+      c.integer :grade
+      c.string :birthdate
+    end
   end
 end
